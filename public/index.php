07d2bfac7f7a405e0d744d6990e5c9801b766f96
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2020-01-12 19:59:31 +0700

    initial commit

--- public/index.php
+++ public/index.php
@@ -1,37 +1,35 @@
 <?php
 use Slim\App;
 use Slim\Container;
+use Slim\HttpCache\CacheProvider;
+use Slim\HttpCache\Cache;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\PhpRenderer as Views;
 use Ayesh\InstagramDownload\InstagramDownload;
-
 require '../vendor/autoload.php';
 
 $config = [
     'settings' => [
-        'displayErrorDetails' => true
+        'displayErrorDetails' => true,
+        'routerCacheFile' => '../cache/cache.php'
     ]
 ];
 
 $container = new Container($config);
 $app = new App($container);
 $container = $app->getContainer();
-
+// Add container to the application
 $container['view'] = new Views('../templates/');
+$container['cache'] = new CacheProvider();
+// Add middleware to the application
+$app->add(new Cache('public', 86400));
 
 $app->get(
     '/',
     function (Request $request, Response $response) {
         $view = $this->get('view');
         $view->render($response, 'home.html');
-    }
-);
-$app->get(
-    '/download-instagram',
-    function (Request $request, Response $response) {
-        $view = $this->get('view');
-        return $view->render($response, 'instagram.html');
     }
 );
 $app->post(
@@ -45,27 +43,21 @@ $app->post(
             $type = $ig->getType();
             return $view->render(
                 $response,
-                'instagram-success.php',
+                'success.php',
                 [
                     'type' => $type,
                     'url' => $url
                 ]
             );
         } else {
-            return $view->render($response, 'instagram-failed.html');
+            return $view->render($response, 'failed.html');
         }
     }
 );
 $app->get(
-    '/download-youtube',
-    function (Request $request, Response $response) {
-        $this->view->render($response, 'youtube.html');
-    }
-);
-$app->get(
-    '/download-facebook',
-    function (Request $request, Response $response) {
-        $this->view->render($response, 'facebook.html');
+    '/download-{type}',
+    function (Request $request, Response $response, $args) {
+        $this->view->render($response, $args['type'] . '.html');
     }
 );
 $app->run();
